fbbe2c7818d64107767c7ca34c3196732a7cbd17
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-10-05 11:44:36 -0400

    asd

--- ChatbotProject/src/chatbotProject/ChatbotYonathan.java
+++ ChatbotProject/src/chatbotProject/ChatbotYonathan.java
@@ -68,7 +68,7 @@ public class ChatbotYonathan implements Topic {
 		 int leftside = 0;
 		while(psn<s.length()) {
 			 
-			  // makes a substring that is  checked against every item in alhabetsoup
+			  // makes a substri ng that is  checked against every item in alhabetsoup
 			  
 			  
 				  if(s.substring(psn,psn+1).equals(" ")) {
